427d7f6c8224467dee76eaebf3cf68200c7ab514
Amarillia <Amarillia@users.noreply.github.com>
2021-08-10 13:37:59 +0100

    updated style, functionality to review typed text

--- markdown_previewer/src/AppMain.js
+++ markdown_previewer/src/AppMain.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
 class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: "",
+            output: ""
+        };
+        // Bind the event handlers
+        this.textInput = this.textInput.bind(this);
+    }
+
+
+    textInput(e) {
+        const preview = document.getElementById("preview");
+        preview.innerHTML = e.target.value;
+    }
+
     render() {
         return (
             <div id="main">
                 <section className="editorSection">
                     <label>Editor</label>
-                    <textarea id="editor" rows="20" cols="50">
+                    <textarea id="editor" rows="20" cols="50" onChange={this.textInput}>
 
                     </textarea>
                 </section>
